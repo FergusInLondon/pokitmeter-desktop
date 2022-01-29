194ccf845c2ac9367f7e378ee2e918aa18899b57
Fergus In London <fergus@fergus.london>
2022-01-29 17:15:03 +0000

    graceful termination when initiated via terminal

--- pokitmeter_desktop/__init__.py
+++ pokitmeter_desktop/__init__.py
@@ -1,11 +1,13 @@
+import signal
 import sys
 
 from PyQt5 import QtWidgets
 
 from .main_gui import MainGui
 
-
 def run():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainGui() # Not entirely sure why the reference is kept?
     app.exec_()
